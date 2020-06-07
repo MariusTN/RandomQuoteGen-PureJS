f4bdda84ffdd3b6871410a06fe618d4eecf5b87b
MariusTN <pulsemusic8@gmail.com>
2020-06-07 17:01:44 +0100

    HTML/CSS and the backbone of JS

---
+++ app.js
@@ -0,0 +1,14 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    event.preventDefault();
+            //Select Button
+            let button = document.querySelector("#quoteButton")
+            //Event Listener to "button"
+            button.addEventListener("click", (event2) => {
+                
+                //prevent browser default action
+                event2.preventDefault();
+                //def the containers of the info we target
+            let quote = document.querySelector("#quoteContent p");
+            let quoteGenius = document.querySelector("#quoteAuthor");              
+            });
+        });
